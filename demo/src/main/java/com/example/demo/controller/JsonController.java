0630fbcdf92a2d154ae0d39e61d78c5a40cad042
admin <admin@192.168.11.105>
2023-11-30 22:12:54 +0900

    2023.11.30 React-Native 向け コントローラ修正

--- demo/src/main/java/com/example/demo/controller/JsonController.java
+++ demo/src/main/java/com/example/demo/controller/JsonController.java
@@ -5,14 +5,25 @@ import java.util.Map;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.LoginRequest;
 
 @RestController
+@RequestMapping("/J") //JSONで受けたるための指定
 public class JsonController {
-  @GetMapping("/json")
+  @GetMapping("/get")
   public Map<String, String> hello(@ModelAttribute LoginRequest loginRequest, Model model) {
+	  System.out.println("loginRequest.name="+loginRequest.getName());
+    return Map.of("msg", loginRequest.getName()+"さん、こんにちは。");
+  }
+  @PostMapping("/post")
+ // public Map<String, String> hello_post(@ModelAttribute LoginRequest loginRequest, Model model) {
+  //JSON形式で受信する場合こっち @RequestBody LoginRequest loginRequest
+  public Map<String, String> hello_post(@RequestBody LoginRequest loginRequest, Model model) {
 	  System.out.println("loginRequest.name="+loginRequest.getName());
     return Map.of("msg", loginRequest.getName()+"さん、こんにちは。");
   }
